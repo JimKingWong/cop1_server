34429efe87d2390ad0cd40712cccab83b65423a9
wong <wangjijian1994@gmail.com>
2025-09-18 15:01:01 -0300

    1

--- application/common.php
+++ application/common.php
@@ -87,7 +87,7 @@ if (!function_exists('cdnurl')) {
     function cdnurl($url, $domain = false)
     {
         $regex = "/^((?:[a-z]+:)?\/\/|data:image\/)(.*)/i";
-        $env = is_local_env() ? 'localcdnurl' : 'cdnurl';
+        $env = 'cdnurl';
         $cdnurl = \think\Config::get('upload.' . $env);
         if (is_bool($domain) || stripos($cdnurl, '/') === 0) {
             $url = preg_match($regex, $url) || ($cdnurl && stripos($url, $cdnurl) === 0) ? $url : $cdnurl . $url;
@@ -621,4 +621,29 @@ function containsChinese($str)
 function is_local_env()
 {
     return in_array($_SERVER['SERVER_ADDR'], ['127.0.0.1', 'localhost']) ? true : false;;
+}
+
+function http_post($url, $data)
+{
+    $headers = array('Content-Type: application/x-www-form-urlencoded');
+    $curl = curl_init(); // 启动一个CURL会话
+    curl_setopt($curl, CURLOPT_URL, $url); // 要访问的地址
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0); // 对认证证书来源的检查
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0); // 从证书中检查SSL加密算法是否存在
+    curl_setopt($curl, CURLOPT_USERAGENT, ''); // 模拟用户使用的浏览器
+    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1); // 使用自动跳转
+    curl_setopt($curl, CURLOPT_AUTOREFERER, 1); // 自动设置Referer
+    curl_setopt($curl, CURLOPT_POST, 1); // 发送一个常规的Post请求
+    curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data)); // Post提交的数据包
+    curl_setopt($curl, CURLOPT_TIMEOUT, 30); // 设置超时限制防止死循环
+    curl_setopt($curl, CURLOPT_HEADER, 0); // 显示返回的Header区域内容
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); // 获取的信息以文件流的形式返回
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    $result = curl_exec($curl); // 执行操作
+    if (curl_errno($curl)) {
+        echo 'Errno' . curl_error($curl);//捕抓异常
+    }
+    curl_close($curl); // 关闭CURL会话
+//        echo($result);
+    return $result;
 }
